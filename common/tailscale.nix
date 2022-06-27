321a296edd5900abe41663e617a935004c7f618f
Matthew Ng <ng2matthew@gmail.com>
2022-06-27 23:06:10 +1000

    feat: update tailscale dnsmasq config

--- common/tailscale.nix
+++ common/tailscale.nix
@@ -23,10 +23,15 @@
       filterwin2k
       no-resolv
 
+      # Send query to all servers
+      # all-server
+
+      # no-negcache
+
       interface=lo
 
+      server=/tailscale.com/1.1.1.1
       server=100.74.157.60
-      server=1.1.1.1
     '';
   };
 
